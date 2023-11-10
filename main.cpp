165ee621a928dfe4c9f581a79d94610d90d98f41
Michael Turner <mjt6yf@umsystem.edu>
2023-11-10 11:05:49 -0600

    Upload New File

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "gal.h"
+using namespace std;
+
+int main() {
+  srand(715);
+  point player_x, player_y, alien_x, alien_y, power_x, power_y, guess_x, guess_y, player_last_x,player_last_y;
+  int humanLives = 6;
+  int alienLives = 12;
+  int bullets = 20;
+  int round = 0;
+  int immunity_duration = 0;
+  string powerUp;
+
+  cout << "Welcome to GalDef - The Galactic Defender! \nThe game starts now!\nYou have 5 lives and 15 bullets to begin with, the alien has 10 lives and will try to attack you! Goodluck, Let the game begin…!!!\n" << endl;
+
+  do{
+    cout << "\nRound "<<++round << endl;
+    generatePlayerPosition(player_x, player_y);
+    generateAlienPosition(alien_x, alien_y);
+    while( (player_x.m_XCoord == alien_x.m_XCoord) && (player_y.m_YCoord == alien_y.m_YCoord)){ //make sure Player and alien arent on same spot
+      generateAlienPosition(alien_x, alien_y);
+    }
+    generatePowerUpPosition(power_x, power_y);
+    
+    for(int i=0; i<2; i++){ //runs shoot alien function 2 times 
+      shootAlien(guess_x, guess_y, alien_x, alien_y, alienLives, bullets,humanLives);
+    }
+    acquirePowerUp(power_x, power_y,  player_x, player_y, humanLives, bullets, immunity_duration, powerUp); //checks if player is on the same spot as the power up
+if(round != 1){ //if not the first round, the alien gets a turn to damage the player based on its last position
+  shootPlayer(player_last_x, player_last_y, alien_x, alien_y, humanLives, immunity_duration);
+}
+    
+    displayGameStatus(humanLives, alienLives, bullets, powerUp,player_x, player_y, alien_x, alien_y);
+  }while (humanLives >0 && alienLives >0 & bullets >0);
+  
+  if(humanLives ==0){
+    cout << "You died, The alien continues to roam and terrorize the galaxy!";
+    return 0;
+  }else if(alienLives == 0){
+    cout << "The alien has been defeated! Congratulations, player! You Saved the galaxy! \nThanks for playing GalDef!";
+    return 0;
+  }else if(bullets == 0){
+  cout << "You ran out of bullets, The alien continues to roam and terrorize the galaxy!";
+  return 0;
+  }
+  return 0;
+}
